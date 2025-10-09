bf13ee8a98ff5f6b5fc82f2cad5cc92734de60b4
GabrielTeixeiraFerreira <scorpigabriel@gmail.com>
2025-10-09 14:30:46 -0300

    docs🗂️: Mudança de lugar

--- libs/Exception.py
+++
@@ -1,5 +0,0 @@
-class Http_Exception(Exception):
-    def __init__(self, codigo, mensagem):
-        self.codigo = codigo
-        self.mensagem = mensagem
-        super().__init__(f"[{codigo}] {mensagem}")
